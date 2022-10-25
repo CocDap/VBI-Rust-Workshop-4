d890130aadc39bef39cb80b644520e75527b4550
CocDap <hodinhdung1997@gmail.com>
2022-10-25 10:28:47 +0900

    chore: remove target

--- complex-type-exercise/target/debug/deps/complex_type_exercise-6a67ddc3804f2474.d
+++
@@ -1,5 +0,0 @@
-/Users/hodung/VBI-substrate-4/complex-type-exercise/target/debug/deps/complex_type_exercise-6a67ddc3804f2474.rmeta: src/main.rs
-
-/Users/hodung/VBI-substrate-4/complex-type-exercise/target/debug/deps/complex_type_exercise-6a67ddc3804f2474.d: src/main.rs
-
-src/main.rs:
--- complex-type-exercise/target/debug/deps/complex_type_exercise-c80ca7450c6d23f4.d
+++
@@ -1,5 +0,0 @@
-/Users/hodung/VBI-substrate-4/complex-type-exercise/target/debug/deps/complex_type_exercise-c80ca7450c6d23f4.rmeta: src/main.rs
-
-/Users/hodung/VBI-substrate-4/complex-type-exercise/target/debug/deps/complex_type_exercise-c80ca7450c6d23f4.d: src/main.rs
-
-src/main.rs:
